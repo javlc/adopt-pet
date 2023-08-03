fcacd2e25c542ff701cd77ea58bcdf7506268438
Javier Lopez C <javlc.dev@gmail.com>
2023-08-03 16:57:30 -0400

    FEM-001 round-4 create Augment search comp w/ elements.

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
   const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
   return (
     <div className="search-params">
       <form>
@@ -13,6 +15,20 @@ const SearchParams = () => {
             placeholder="Location"
             onChange={(e) => setLocation(e.target.value)}
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            placeholder="Animal"
+            onChange={(e) => setAnimal(e.target.value)}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal}>{animal}</option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
